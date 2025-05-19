47c91b0e1a71bf18b14a572821b032e55b202d07
YingqiDuan <duanyingqi157@outlook.com>
2025-05-18 19:49:58 -0700

    first commit

---
+++ src/main/java/com/microservice/user_behaviour/UserBehaviourApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.user_behaviour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBehaviourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBehaviourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/user_behaviour/UserBehaviourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.user_behaviour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBehaviourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
